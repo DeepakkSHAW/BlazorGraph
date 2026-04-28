2b97517ea10d698d82e49743cc2e2aa8922e7a1a
Deepak SHAW <deepakk_2k@hotmail.com>
2026-04-28 23:27:48 +1000

    Add project files.

---
+++ BlazorGraph/Services/ChartService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.JSInterop;
+
+public class ChartService
+{
+    private readonly IJSRuntime _js;
+
+    public ChartService(IJSRuntime js)
+    {
+        _js = js;
+    }
+
+    public async Task RenderLineChart(string id, string[] labels, double[] data)
+    {
+        await _js.InvokeVoidAsync(
+            "chartInterop.renderLineChart",
+            id,
+            labels,
+            data
+        );
+    }
+}
